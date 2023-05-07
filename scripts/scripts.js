75ce2f2c8c557bc53938a06ac7ea5d06a2878a6b
Heidi Chen <heidi@macOS-heidi.local>
2023-05-06 22:00:00 -0700

    https://github.com/adobe/inside-aem/issues/4#issue-1693738866

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -76,10 +76,21 @@ async function loadEager(doc) {
  * Adds the favicon.
  * @param {string} href The favicon URL
  */
+// export function addFavIcon(href) {
+//   const link = document.createElement('link');
+//   link.rel = 'icon';
+//   link.type = 'image/svg+xml';
+//   link.href = href;
+//   const existingLink = document.querySelector('head link[rel="icon"]');
+//   if (existingLink) {
+//     existingLink.parentElement.replaceChild(link, existingLink);
+//   } else {
+//     document.getElementsByTagName('head')[0].appendChild(link);
+//   }
+// }
 export function addFavIcon(href) {
   const link = document.createElement('link');
   link.rel = 'icon';
-  link.type = 'image/svg+xml';
   link.href = href;
   const existingLink = document.querySelector('head link[rel="icon"]');
   if (existingLink) {
@@ -89,7 +100,7 @@ export function addFavIcon(href) {
   }
 }
 
-/**
+  /**
  * Loads everything that doesn't need to be delayed.
  * @param {Element} doc The container element
  */
