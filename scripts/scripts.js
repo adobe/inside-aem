2c97027c4b08d0d640bdd59ced27878647c7c98f
heidichen18 <heidi@adobe.com>
2023-05-09 22:15:05 -0700

    remove commented out codes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -136,18 +136,7 @@ async function loadEager(doc) {
  * Adds the favicon.
  * @param {string} href The favicon URL
  */
-// export function addFavIcon(href) {
-//   const link = document.createElement('link');
-//   link.rel = 'icon';
-//   link.type = 'image/svg+xml';
-//   link.href = href;
-//   const existingLink = document.querySelector('head link[rel="icon"]');
-//   if (existingLink) {
-//     existingLink.parentElement.replaceChild(link, existingLink);
-//   } else {
-//     document.getElementsByTagName('head')[0].appendChild(link);
-//   }
-// }
+
 export function addFavIcon(href) {
   const link = document.createElement('link');
   link.rel = 'icon';
