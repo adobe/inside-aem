04a928a69b630ac42d52bbbcf8e7a730f581e38c
heidichen18 <heidi@adobe.com>
2023-05-18 22:58:48 -0700

    edit search

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -128,15 +128,10 @@ export default async function decorate(block) {
       navTools.innerHTML = `
       <div class="nav-search">
         <button type="button" aria-controls="nav" aria-label="Open navigation">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-            <path d="M16.9,15.5c2.4-3.2,2.2-7.7-0.7-10.6c-3.1-3.1-8.1-3.1-11.3,0c-3.1,3.2-3.1,8.3,0,11.4
-              c2.9,2.9,7.5,3.1,10.6,0.6c0,0.1,0,0.1,0,0.1l4.2,4.2c0.5,0.4,1.1,0.4,1.5,0c0.4-0.4,0.4-1,0-1.4L16.9,15.5
-              C16.9,15.5,16.9,15.5,16.9,15.5L16.9,15.5z M14.8,6.3c2.3,2.3,2.3,6.1,0,8.5c-2.3,2.3-6.1,2.3-8.5,0C4,12.5,4,8.7,6.3,6.3
-              C8.7,4,12.5,4,14.8,6.3z"/>
-          </svg>
+          ${navTools.innerHTML}
         </button>
-      </div>
-
+      </div>`;
+      navTools.innerHTML += `
       <a class="nav-logo" href="https://www.adobe.com/" aria-label="Adobe">
         <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 133.46 118.11"><defs><style>.cls-1{fill:#fa0f00;}</style></defs><polygon class="cls-1" points="84.13 0 133.46 0 133.46 118.11 84.13 0"/><polygon class="cls-1" points="49.37 0 0 0 0 118.11 49.37 0"/><polygon class="cls-1" points="66.75 43.53 98.18 118.11 77.58 118.11 68.18 94.36 45.18 94.36 66.75 43.53"/></svg>
       </a>
