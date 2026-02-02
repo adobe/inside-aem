faacab7a1ac3d97b06db7a2095ced75c8d478888
dosteinacher <118809662+dosteinacher@users.noreply.github.com>
2026-02-02 08:29:03 +0100

    debug: Add detailed error logging to gnav init
    - Log actual error object when gnav creation fails
    - Will help diagnose why gnav doesn't load on live site

--- blocks/gnav/gnav.js
+++ blocks/gnav/gnav.js
@@ -373,7 +373,9 @@ export default async function init(blockEl) {
         const gnavDoc = parser.parseFromString(html, 'text/html');
         const gnav = new Gnav(gnavDoc.body, blockEl);
         gnav.init();
-      } catch {
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(`Could not create global navigation. ${url}`, error);
         debug(`Could not create global navigation.${url}`);
       }
     }
